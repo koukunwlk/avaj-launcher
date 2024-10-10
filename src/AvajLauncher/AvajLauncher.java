c0c103b45668720492731bb6c04f39504fe8e5f9
Moacir Amaro da silva junior <mamaro-d@c2r4s4.42porto.com>
2024-10-10 15:52:16 +0100

    feat: add coordinate class

--- src/AvajLauncher/AvajLauncher.java
+++ src/AvajLauncher/AvajLauncher.java
@@ -1,5 +1,9 @@
 public class AvajLauncher{
+
     public static void main(String args[]) {
+        Coordinates coor = new Coordinates(10, 15, 20);
+        String str = coor.toString();
+        System.out.println(str);
         System.out.println("hello world");
     }
 }
--- src/AvajLauncher/Coordinates.java
+++ src/AvajLauncher/Coordinates.java
@@ -1,11 +1,10 @@
 public class Coordinates {
-    private:
-        int longitude;
-        int latitude;
-        int height;
+        private int longitude;
+        private int latitude;
+        private int height;
 
-        public:
-            Coordinates(int p_longitude, int p_latitude, int p_height) {
+        
+        public Coordinates(int p_longitude, int p_latitude, int p_height) {
                 this.longitude = p_longitude;
                 this.latitude = p_latitude;
                 this.height = p_height;
@@ -13,32 +12,36 @@ public class Coordinates {
         
             // Getters
 
-            public int getLongitude() {
-                return this.longitude;
-            }
-            
-            public int getLatitude() {
-                return this.latitude;
-            }
-            
-            public int getHeight() {
-                return this.height;
-            }
-            
+        public int getLongitude() {
+            return this.longitude;
+        }
+        
+        public int getLatitude() {
+            return this.latitude;
+        }
+        
+        public int getHeight() {
+            return this.height;
+        }
+        
 
-            // Setters
+        // Setters
 
-            public void setLongitude(int p_longitude) {
-                this.longitude = p_longitude;
-            }
+        public void setLongitude(int p_longitude) {
+            this.longitude = p_longitude;
+        }
 
-            public void setLatitude(int p_latitude) {
-                this.latitude =  p_latitude;
-            }
+        public void setLatitude(int p_latitude) {
+            this.latitude =  p_latitude;
+        }
 
-            public void setHeight(int p_height) {
-                this.height = p_height;
-            }
+        public void setHeight(int p_height) {
+            this.height = p_height;
+        }
+
+        public String toString() {
+            return "longitude: " + this.longitude + " latitude: " + this.latitude + " height: " + this.height;
+        }
 
             
 
